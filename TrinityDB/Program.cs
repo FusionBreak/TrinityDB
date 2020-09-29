f9941cb30d96a7b834cdfead302c8dc53ade9d76
Michele Beckmann <m.beckmann@live.de>
2020-09-29 19:36:19 +0200

    project init

---
+++ TrinityDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrinityDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
